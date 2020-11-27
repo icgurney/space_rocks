d2570096c131db6ddf23523a6abf83a6f36c8c7c
Ian Gurney <ianclaytongurney@gmail.com>
2020-11-27 14:16:05 -0600

    Form events

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,34 @@
-export default function HomePage() {
-  return <div>Welcome to Next.js!!!!!!!</div>;
-}
+import { useRouter } from "next/router";
+import { ChangeEvent, FormEvent, useState } from "react";
+
+const HomePage = () => {
+  const router = useRouter();
+  const [inputField, setInputField] = useState({
+    date: "",
+  });
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement;
+    setInputField({ date: target.value });
+  };
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    router.push(`/apod/${inputField.date}`);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="date">Enter a date: </label>
+        <input
+          type="date"
+          name="date"
+          id="date"
+          onChange={handleChange}
+          // onBlur={handleBlur}
+        />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
+
+export default HomePage;
